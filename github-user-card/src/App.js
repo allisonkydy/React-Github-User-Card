b5b2a791face6779819fc57305c081fd0bf30a2a
allisonkydy <allisnelly@gmail.com>
2019-09-03 14:41:25 -0500

    fetched user data, set state for user and userData

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -1,14 +1,27 @@
 import React from 'react';
+import axios from 'axios';
 import './App.css';
 
-class App extends React.Component() {
+class App extends React.Component {
   constructor() {
     super();
     this.state = {
-
+      user: 'allisonkydy',
+      userData: []
     }
   }
-  
+
+  componentDidMount() {
+    axios.get(`https://api.github.com/users/${this.state.user}`)
+      .then(response => {
+        console.log(response);
+        this.setState(() => ({ userData: response.data}));
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  }
+
   render() {
       return (
       <div className="App">
