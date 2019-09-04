628c5d4e1309fb4f1718a672a657da05b725c9b4
allisonkydy <allisnelly@gmail.com>
2019-09-03 22:39:11 -0500

    added search functionality

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -15,21 +15,15 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-    // fetch data for current user
-    axios.get(`https://api.github.com/users/${this.state.user}`)
-      .then(response => {
-        // console.log(response);
-        // then put the data into userData state
-        this.setState(() => ({ userData: response.data }));
-      })
-      .catch(error => {
-        console.log(error);
-      });
+    this.getUser();
+    this.setState({ user: "" })
   }
 
   componentDidUpdate(prevProps, prevState) {
     // if the userData is updated
     if (this.state.userData !== prevState.userData) {
+      // reset followersData
+      this.setState(() => ({ followersData: [] }))
       // update followersData
       axios.get(this.state.userData.followers_url)
         .then(response => {
@@ -48,18 +42,51 @@ class App extends React.Component {
     }
   }
 
+  getUser = () => {
+    // fetch data for current user
+    axios.get(`https://api.github.com/users/${this.state.user}`)
+      .then(response => {
+        // console.log(response);
+        // then put the data into userData state
+        this.setState({ userData: response.data });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value})
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.getUser();
+    this.setState({ user: "" })
+  }
+
   render() {
     return (
       <div className="App">
         <h1 className="main-heading">github user cards</h1>
-        <div class="card-container">
-          <div class="user-card">
+        <form onSubmit={this.handleSubmit}>
+          <input 
+            type="text" 
+            placeholder="enter a username" 
+            value={this.state.user}
+            name="user"
+            onChange={this.handleChange}
+          />
+          <button type="submit">search</button>
+        </form>
+        <div className="card-container">
+          <div className="user-card">
             <h2 className="main-heading">User: </h2>
             <Card userData={this.state.userData}/>
           </div>
-          <div class="follower-container">
+          <div className="follower-container">
             <h2 className="main-heading">Followers: </h2>
-            <div class="follower-cards">
+            <div className="follower-cards">
               {this.state.followersData.map(follower => {
                 return <Card userData={follower} key={follower.id} />
               })}
